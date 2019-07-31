606fd40e090e4dbb8123952c04239794285c0201
Abaab-Ahmed <abaab_1989@hotmail.com>
2019-07-31 04:56:08 +0100

    GetIPLogs.ps1

---
+++ GetIPLogs.ps1
@@ -0,0 +1,8 @@
+﻿ $servers = 'INX-srv-APPL145','INX-srv-APPL146','RDB-srv-APPL145','RDB-srv-APPL146'
+
+foreach ($server in $servers)
+{
+    Get-Content \\$Server\LOGS\WEB\INSTRUCTIONPROCESSOR.CITYINDEX.CO.UK\AUDIT.LOG -tail 1
+
+    }  
+ 
